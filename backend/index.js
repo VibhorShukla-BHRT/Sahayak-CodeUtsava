8f1323fcdf66c1b5098bfaa288d8d7bde1eac040
Maxcillius <alphaviperchamber@hotmail.com>
2024-10-26 11:59:28 +0530

    Initialization and smart-contract

--- backend/index.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-
-const app = express();
-
-app.listen(4000,(req,res)=>{
-    console.log("server is running on port 4000");
-})
--- backend/routes/auth.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const {signUp,login} = require("../Controller/Auth");
-
-router.post("/api/v1/signup",signUp);
-router.post("/api/v1/login",login);
-
-export default router;
